02f3952705d4bdf57f7c5f146eae0887a4d166d8
bahar-sadeghzadeh <baharsadeghzade@gmail.com>
2024-12-31 22:47:50 +0100

    fix address

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,4 @@
-export const ADDRESS =
-  "Veresegyház Dental Fogászat, Veresegyház, Fő út 73, 2112";
+export const ADDRESS = "2112 Veresegyház Fő út 73";
 export const PHONE_NUMBER = "+36 70 536 0300";
 export const EMAIL = "multipalladiumdental@gmail.com";
 // export const WORKING_HOURS = "Mon - Fri : 09.00 AM - 09.00 PM";
