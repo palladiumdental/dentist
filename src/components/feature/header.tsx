de48e38ea0acfd197326ddd2b274f158d19a8d9d
bahar-sadeghzadeh <baharsadeghzade@gmail.com>
2025-01-29 21:22:26 +0100

    change core values

--- src/components/feature/header.tsx
+++ src/components/feature/header.tsx
@@ -16,7 +16,15 @@ const Header: React.FC = () => {
       <div className="row g-0 align-items-center flex-column-reverse flex-lg-row">
         <div className="col-lg-6 p-5 wow fadeIn" data-wow-delay="0.1s">
           <h1 className="display-4 text-white mb-5">
-            <Localize text="a1" isFirstLetterCapital={true} />
+            <div style={{ marginBottom: "30px" }}>
+              <Localize text="your smile" isFirstLetterCapital={true} />
+            </div>
+            <div style={{ marginBottom: "30px" }}>
+              <Localize text="our care" isFirstLetterCapital={true} />
+            </div>
+            <div>
+              <Localize text="your confidence" isFirstLetterCapital={true} />
+            </div>
           </h1>
         </div>
         <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
